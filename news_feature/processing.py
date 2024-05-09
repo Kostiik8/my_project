d17ac369b6cad49ff7cfc9095f9d6445e61f01e6
Artyukhin Konstantin <k89161273297@mail.ru>
2024-05-09 13:18:15 +0300

    fix mistake

--- news_feature/processing.py
+++ news_feature/processing.py
@@ -1,9 +1,9 @@
 from typing import Any, Dict
 
 
-def dict_list(list_of_dicts: list[dict[str, Any]], state_value: Any) -> list[dict[str, Any]]:
+def dict_list(list_of_dicts: list[dict[str, Any]], state_value: Any = 'EXECUTED') -> list[dict[str, Any]]:
     """Функция принимает список словарей и возвращает
-       новый словарь с указанным значением.
+       отсортированный по значению список словарей
     """
 
     new_list = []
@@ -13,29 +13,22 @@ def dict_list(list_of_dicts: list[dict[str, Any]], state_value: Any) -> list[dic
     return new_list
 
 
-state_value = 'CANCELED'
 list_of_dicts = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
                  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
                  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
                  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
-result = dict_list(list_of_dicts, state_value)
-print(result)
 
-
-def dictionary(list_dicts: list[Dict[str, Any]], reverse: bool = True) -> list[Dict[str, Any]]:
+def dictionary(sort_dictionaries: list[Dict[str, Any]], reverse: bool = True) -> list[Dict[str, Any]]:
     """Функция принимает список словарей и возвращает
        отсортированный список по убыванию дат
     """
 
-    new_list = sorted(list_dicts, key=lambda x: x['date'], reverse=reverse)
+    new_list = sorted(sort_dictionaries, key=lambda x: x['date'], reverse=True)
     return new_list
 
 
-list_dicts = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-              {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-              {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-              {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-
-result = dictionary(list_dicts, reverse=True)
-print(result)
+sort_dictionaries = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]

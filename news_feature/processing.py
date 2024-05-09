b7f16400961b3d524ed8785d054193d2e78e76b2
Artyukhin Konstantin <k89161273297@mail.ru>
2024-05-09 22:27:35 +0300

    fix mistake and delete test code, and rename func

--- news_feature/processing.py
+++ news_feature/processing.py
@@ -1,13 +1,13 @@
 from typing import Any, Dict
 
 
-def dict_list(list_of_dicts: list[dict[str, Any]], state_value: Any = 'EXECUTED') -> list[dict[str, Any]]:
+def dict_list(filter_operations_list: list[dict[str, Any]], state_value: Any = 'EXECUTED') -> list[dict[str, Any]]:
     """Функция принимает список словарей и возвращает
        отсортированный по значению список словарей
     """
 
     new_list = []
-    for dict_ in list_of_dicts:
+    for dict_ in filter_operations_list:
         if state_value in dict_.values():
             new_list.append(dict_)
     return new_list

8888aca7cb097f040b5d9d2336850de0e97bd96d
Artyukhin Konstantin <k89161273297@mail.ru>
2024-05-10 21:26:20 +0300

    rename func

--- news_feature/processing.py
+++ news_feature/processing.py
@@ -1,20 +1,21 @@
 from typing import Any, Dict
 
 
-def dict_list(filter_operations_list: list[dict[str, Any]], state_value: Any = 'EXECUTED') -> list[dict[str, Any]]:
+def filter_operations_list(list_of_dicts: list[dict[str, Any]], state_value: Any = 'EXECUTED') -> list[dict[str, Any]]:
     """Функция принимает список словарей и возвращает
        отсортированный по значению список словарей
     """
 
     new_list = []
-    for dict_ in filter_operations_list:
+    for dict_ in list_of_dicts:
         if state_value in dict_.values():
             new_list.append(dict_)
     return new_list
 
 
-def dictionary(sort_dictionaries: list[Dict[str, Any]], reverse: bool = True) -> list[Dict[str, Any]]:
+def sort_operations_list(sort_dictionaries: list[Dict[str, Any]], reverse: bool = True) -> list[Dict[str, Any]]:
     """Функция принимает список словарей и возвращает
+       отсортированный список по убыванию дат
        список словарей
     """
 
